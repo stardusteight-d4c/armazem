8392769eb7c89f1dd66aba0db8061064c80bade8
Gabriel Sena <stardusteight.d4cc@gmail.com>
2022-11-26 20:10:14 -0300

    like post in trending post

--- web/src/components/feed/integrate/RecentPost.tsx
+++ web/src/components/feed/integrate/RecentPost.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   likePost,
   postMetadataById,
@@ -8,12 +8,12 @@ import {
   unsharePost,
 } from '../../../services/api-routes'
 import { useAppDispatch, useAppSelector } from '../../../store/hooks'
+import { success } from '../../Toasters'
+import { getCurrentUserAccount } from '../../../store/reducers/current-user-data'
+
 import TimeAgo from 'timeago-react'
 import * as timeago from 'timeago.js'
 import en_short from 'timeago.js/lib/lang/en_short'
-import { success } from '../../Toasters'
-import { getCurrentUserAccount } from '../../../store/reducers/current-user-data'
-import { askToRequestAgain } from '../../../store'
 timeago.register('en_short', en_short)
 
 interface Props {
@@ -23,15 +23,16 @@ interface Props {
 export const RecentPost = ({ post }: Props) => {
   const currentUser = useAppSelector((state) => state.armazem.currentUser)
   const currentAccount = useAppSelector((state) => state.armazem.currentAccount)
-  const [authorUser, setAuthorUser] = useState<User | any>([])
+  const [authorUser, setAuthorUser] = useState<User | null>(null)
   const dispatch = useAppDispatch()
-  const [likedByUser, setLikedByUser] = useState<boolean>()
-  const [request, setRequest] = useState(false)
-  const [arrLikesCopy, setArrLikesCopy] = useState<any>()
+  const [likedByUser, setLikedByUser] = useState<boolean>(false)
+  const [likeRecalculation, setLikeRecalculation] = useState(false)
+  const [arrLikesCopy, setArrLikesCopy] = useState<Array<{ by: string }>>([])
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
     setArrLikesCopy([...post.likes])
-  }, [])
+  }, [post])
 
   useEffect(() => {
     if (post._id) {
@@ -58,8 +59,7 @@ export const RecentPost = ({ post }: Props) => {
           if (index !== -1) {
             arrLikesCopy.splice(index, 1)
           }
-          console.log('arrLikesCopy remove realizado', arrLikesCopy)
-          setRequest(!request)
+          setLikeRecalculation(!likeRecalculation)
         })
         .catch((error) => console.log(error.toJSON()))
     } else {
@@ -69,13 +69,16 @@ export const RecentPost = ({ post }: Props) => {
           postId: post?._id,
         })
         .then(() => {
-          arrLikesCopy.push({ by: currentUser?._id })
-
-          setRequest(!request)
+          arrLikesCopy.push({ by: currentUser!._id })
+          setLikeRecalculation(!likeRecalculation)
         })
         .catch((error) => console.log(error.toJSON()))
     }
   }
+
+  setTimeout(() => {
+    setLoading(false)
+  }, 1000)
 
   const handleSharePost = async () => {
     if (!isSharedPost) {
@@ -104,21 +107,16 @@ export const RecentPost = ({ post }: Props) => {
   }
 
   useEffect(() => {
-    if (arrLikesCopy) {
-      const verificationResult = arrLikesCopy.map(
-        (like: { by: string | undefined }) => {
-          if (like.by == currentUser?._id) {
-            return true
-          } else {
-            return false
-          }
-        }
-      )
-      console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', likedByUser)
-
-      setLikedByUser(verificationResult.includes(true))
-    }
-  }, [request])
+    const verificationResult = arrLikesCopy.map((like: { by: string }) => {
+      console.log('like.by', like.by)
+      if (like.by === currentUser?._id) {
+        return true
+      } else {
+        return false
+      }
+    })
+    setLikedByUser(verificationResult.includes(true))
+  }, [post, likeRecalculation, loading])
 
   const sharedPosts = currentAccount?.sharedPosts?.map(
     (postShared: any) => postShared.id
@@ -126,43 +124,31 @@ export const RecentPost = ({ post }: Props) => {
   const isSharedPost = currentAccount && sharedPosts?.includes(post?._id)
 
   return (
-    <article className="p-4 inline-block max-h-fit bg-dusk-weak/10 h-fit border border-dawn-weak/20 dark:border-dusk-weak/20 w-full">
+    <article className={style.wrapper}>
       <div>
-        <div className="flex items-center gap-x-2">
-          <img src={authorUser.user_img} className="w-8 h-8 rounded-md" />
-          <h3 className="font-medium text-dawn-weak line-clamp-2">
-            Posted by @{authorUser.username}{' '}
+        <div className={style.authorContainer}>
+          <img src={authorUser?.user_img} className={style.authorImage} />
+          <h3 className={style.postedBy}>
+            Posted by @{authorUser?.username}{' '}
             <TimeAgo datetime={post.createdAt} />
           </h3>
         </div>
-        <h2 className="font-semibold break-all line-clamp-2 text-xl my-1">
-          {post.title}
-        </h2>
-        <p className="line-clamp-[10] font-inter break-all break-words whitespace-pre-wrap">
-          {post.body}
-        </p>
+        <h2 className={style.title}>{post.title}</h2>
+        <p className={style.body}>{post.body}</p>
       </div>
       <div>
-        <div className="h-[1px] w-full my-2 border-b border-b-dawn-weak/20 dark:border-b-dusk-weak/20" />
-        <div className="flex items-center justify-between text-dusk-main dark:text-dawn-main">
-          <div className="flex items-center gap-x-5">
-            <div
-              onClick={handleLikePost}
-              className="flex w-[84px] items-center cursor-pointer"
-            >
-              <i
-                className={`${
-                  likedByUser
-                    ? 'ri-heart-3-fill text-prime-blue'
-                    : 'ri-heart-3-line'
-                }  text-xl pr-1 `}
-              />
+        <div className={style.divider} />
+        <div className={style.postActionsFlexContainer}>
+          <div className={style.firstSectionContainer}>
+            <div onClick={handleLikePost} className={style.likeContainer}>
+              <i className={likedByUser ? style.likedIcon : style.likeIcon} />
               <span className="text-lg">
-                {post.likes.length} {post.likes.length <= 1 ? 'Like' : 'Likes'}
+                {arrLikesCopy.length}{' '}
+                {arrLikesCopy.length <= 1 ? 'Like' : 'Likes'}
               </span>
             </div>
-            <div className="flex items-center cursor-pointer">
-              <i className="ri-discuss-line pr-1 text-xl" />
+            <div className={style.discussionContainer}>
+              <i className={style.discussionIcon} />
               <span className="text-lg">
                 {post.discussions.length}{' '}
                 {post.discussions.length <= 1 ? 'Discussion' : 'Discussions'}
@@ -191,6 +177,20 @@ export const RecentPost = ({ post }: Props) => {
 }
 
 const style = {
+  wrapper: `p-4 inline-block max-h-fit bg-dusk-weak/10 h-fit border border-dawn-weak/20 dark:border-dusk-weak/20 w-full`,
+  authorContainer: `flex items-center gap-x-2`,
+  authorImage: `w-8 h-8 rounded-md`,
+  postedBy: `font-medium text-dawn-weak line-clamp-2`,
+  title: `font-semibold break-all line-clamp-2 text-xl my-1`,
+  body: `line-clamp-[10] font-inter break-all break-words whitespace-pre-wrap`,
+  divider: `h-[1px] w-full my-2 border-b border-b-dawn-weak/20 dark:border-b-dusk-weak/20`,
+  postActionsFlexContainer: `flex items-center justify-between text-dusk-main dark:text-dawn-main`,
+  firstSectionContainer: `flex items-center gap-x-5`,
+  likeContainer: `flex w-[84px] items-center cursor-pointer`,
+  likeIcon: `ri-heart-3-line text-xl pr-1`,
+  likedIcon: `ri-heart-3-fill text-prime-blue text-xl pr-1`,
+  discussionContainer: `flex items-center cursor-pointer`,
+  discussionIcon: `ri-discuss-line pr-1 text-xl`,
   shareContainer: `cursor-pointer flex items-center`,
   shareIcon: `ri-share-box-line pr-1 text-xl`,
 }
